77c9f5856c4ff929f5819b5aebc2d3490e87b069
Nikhil Karanjkar <karanjkarnikhil4@gmail.com>
2020-01-01 15:10:03 +0530

    commiting the repo

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes:Recipe[] = [new Recipe("alfredo","new alfredo dish","https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103"),
+  new Recipe("pizza","special pizza","https://assets.bonappetit.com/photos/5db1ce83358b460009148cb7/3:2/w_1280,c_limit/Basically-Spinach-Tarte-Recipe.jpg")];
+  constructor() { 
+
+  }
 
   ngOnInit() {
   }
